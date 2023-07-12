1c5cbe0e09a00ae4d208b1c1c862272d379ef38b
jackhirschhorn <jackhirschhorn@gmail.com>
2023-07-12 10:05:54 -0400

    barbarian model + palate swap + shaders

---
+++ Goddess spire/Assets/Scripts/palateswap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class palateswap : MonoBehaviour
+{
+	public bool swap = false;
+	public List<Texture2D> tex = new List<Texture2D>();
+	public List<RenderTexture> rtex = new List<RenderTexture>();
+	public Material swapmat;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(swap){
+			for(int i = 0; i < tex.Count; i++){
+				Graphics.Blit(tex[i],rtex[i],swapmat,0); 
+			}
+			swap = false;
+		}
+        
+    }
+	
+}
