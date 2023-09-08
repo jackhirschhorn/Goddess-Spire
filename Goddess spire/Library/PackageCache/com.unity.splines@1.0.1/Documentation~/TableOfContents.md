0ec0e153993d9c64e07c8613b0b01f8e3bb89354
jackhirschhorn <jackhirschhorn@gmail.com>
2023-09-07 23:43:48 -0400

    Delete Goddess spire/Library directory
    don't need it lol

--- Goddess spire/Library/PackageCache/com.unity.splines@1.0.1/Documentation~/TableOfContents.md
+++
@@ -1,4 +0,0 @@
-* [Splines](index.md)
-* [Getting started with Splines](getting-started-with-splines.md)  
-* [Spline Container](spline-container.md) 
-
--- Goddess spire/Library/PackageCache/com.unity.splines@1.0.1/Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Splines.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.Splines.Editor.Debug")]
