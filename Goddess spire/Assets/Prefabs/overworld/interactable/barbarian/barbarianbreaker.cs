0decf1d3244a48e8389fc57506f6eee88aaeeca6
jackhirschhorn <jackhirschhorn@gmail.com>
2023-10-25 22:22:49 -0400

    added sfx pt1

--- Goddess spire/Assets/Prefabs/overworld/interactable/barbarian/barbarianbreaker.cs
+++ Goddess spire/Assets/Prefabs/overworld/interactable/barbarian/barbarianbreaker.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class barbarianbreaker : MonoBehaviour
 {
 	public bool broken = false;
+	public AudioSource sfx;
 	void OnTriggerEnter(Collider col){
 		Debug.Log(col.transform.GetComponent<Animator>().GetBool("barbcharge"));
 		if(col.transform.GetComponent<playercontroller>() && col.transform.GetComponent<playercontroller>().classid == 0 && col.transform.GetComponent<Animator>().GetBool("barbcharge")){
 			broken = true;
+			sfx.Play();
 			transform.GetComponent<Animator>().SetBool("break", true);
 		}
 	}
