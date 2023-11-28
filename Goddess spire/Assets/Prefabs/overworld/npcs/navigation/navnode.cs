01529de78b8d8183ef0f8815536e3c7c84455889
jackhirschhorn <jackhirschhorn@gmail.com>
2023-11-28 15:09:09 -0500

    working on NPC stuff

---
+++ Goddess spire/Assets/Prefabs/overworld/npcs/navigation/navnode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navnode : MonoBehaviour
+{
+	
+	//call in editor, reset nav, test against every node in scene with a raycast, if path is clear record connection and distance.
+	//debug mode that shows connections with debug.line or whatever
+    
+}
