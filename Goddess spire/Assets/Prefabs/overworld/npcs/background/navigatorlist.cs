0186cfa5993ded147eb4c9a42f642abf483fba10
jackhirschhorn <jackhirschhorn@gmail.com>
2023-11-30 04:25:31 -0500

    gotta work gotta work gotta work

---
+++ Goddess spire/Assets/Prefabs/overworld/npcs/background/navigatorlist.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class navigatorlist : MonoBehaviour
+{
+	
+    public List<navnode> connections = new List<navnode>();
+	public List<float> connectionswait = new List<float>();
+}
