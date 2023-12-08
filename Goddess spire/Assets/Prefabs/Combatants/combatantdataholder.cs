6576d04efddf85acada3eaf8eb567207bbc85c54
jackhirschhorn <jackhirschhorn@gmail.com>
2023-12-07 20:10:49 -0500

    did some stuff

---
+++ Goddess spire/Assets/Prefabs/Combatants/combatantdataholder.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class combatantdataholder : MonoBehaviour
+{
+    public List<combatantdata> cd = new List<combatantdata>();
+}
