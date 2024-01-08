2255c22f95b939c172c44e30018234c74b86e6de
jackhirschhorn <jackhirschhorn@gmail.com>
2024-01-08 13:47:40 -0500

    game menu pt1

---
+++ Goddess spire/Assets/Scripts/gamemenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemenu : MonoBehaviour
+{
+    public int menustate = 0;
+	public int menusubstate = 0;
+	// Start is called before the first frame update
+    void Start()
+    {
+        menustate = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
