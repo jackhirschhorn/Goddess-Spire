f64c103c33c8c81adb73478ad770af793fd61fef
jackhirschhorn <jackhirschhorn@gmail.com>
2024-01-18 12:32:29 -0500

    fixed menu animations

--- Goddess spire/Assets/Scripts/gamemenu.cs
+++ Goddess spire/Assets/Scripts/gamemenu.cs
@@ -28,6 +28,7 @@ public class gamemenu : MonoBehaviour
 	
 	public void updatemenutab(int i){
 		menustate = i;
+		anim.SetInteger("lastmenutarg", anim.GetInteger("menutarg"));
 		anim.SetInteger("menutarg", i);
 		for(int i2 = 1; i2 < 5; i2++){
 			transform.GetChild(0).GetChild(0).GetChild(i2).gameObject.SetActive(false);
@@ -38,6 +39,10 @@ public class gamemenu : MonoBehaviour
 	public void updatesubmenutab(int i){
 		menusubstate = i;
 		anim.SetInteger("submenutarg", i);
+		if(menusubstate == 1){
+			transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
+			transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+		}
 		/*for(int i2 = 1; i2 < 5; i2++){
 			transform.GetChild(0).GetChild(0).GetChild(i2).gameObject.SetActive(false);
 			if(i2 == i)transform.GetChild(0).GetChild(0).GetChild(i2).gameObject.SetActive(true);
@@ -48,19 +53,26 @@ public class gamemenu : MonoBehaviour
 		optionsmenustate = i;
 		anim.SetInteger("optsmenutarg", i);
 		for(int i2 = 1; i2 < 5; i2++){
-			transform.GetChild(0).GetChild(1).GetChild(i2).gameObject.SetActive(false);
-			if(i2 == i)transform.GetChild(0).GetChild(1).GetChild(i2).gameObject.SetActive(true);
+			transform.GetChild(0).GetChild(1).GetChild(0).GetChild(i2).gameObject.SetActive(false);
+			if(i2 == i)transform.GetChild(0).GetChild(1).GetChild(0).GetChild(i2).gameObject.SetActive(true);
 		}
 	}
 	
 	public void callmenu(){
-		menuon = !menuon;
-		if(menuon){
-			Time.timeScale = 0;
-			transform.GetChild(0).gameObject.SetActive(true);
+		if(menusubstate == 0){
+			menuon = !menuon;
+			if(menuon){
+				Time.timeScale = 0;
+				transform.GetChild(0).gameObject.SetActive(true);
+			} else {
+				Time.timeScale = 1;
+				transform.GetChild(0).gameObject.SetActive(false);
+			}
 		} else {
-			Time.timeScale = 1;
-			transform.GetChild(0).gameObject.SetActive(false);
+			menusubstate = 0;
+			anim.SetInteger("submenutarg", 0);
+			transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
+			transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
 		}
 		
 	}
