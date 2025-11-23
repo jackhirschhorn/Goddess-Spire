5f064f7fa82841aeb981b8d24ec744e2105834b8
jackhirschhorn <jackhirschhorn@gmail.com>
2025-11-23 14:11:47 -0500

    more model work

--- Goddess spire/Assets/Assets/2D/character sprites/spriteswapper.cs
+++ Goddess spire/Assets/Assets/2D/character sprites/spriteswapper.cs
@@ -8,10 +8,11 @@ public class spriteswapper : MonoBehaviour
     public Sprite[] sprites = new Sprite[0];
 	public Image img;
 	public int spritenum = 0;
+	public int indexoffset = 0;
 
     // Update is called once per frame
     void Update()
     {
-        img.sprite = sprites[spritenum];
+        img.sprite = sprites[spritenum+indexoffset];
     }
 }
