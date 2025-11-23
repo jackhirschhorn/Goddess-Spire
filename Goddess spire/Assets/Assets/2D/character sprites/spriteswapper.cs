5175f9284f9d2388b80e44d59d8d0c7466c5fa6e
jackhirschhorn <jackhirschhorn@gmail.com>
2025-11-23 12:43:40 -0500

    added sprite swapper

---
+++ Goddess spire/Assets/Assets/2D/character sprites/spriteswapper.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class spriteswapper : MonoBehaviour
+{
+    public Sprite[] sprites = new Sprite[0];
+	public Image img;
+	public int spritenum = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.sprite = sprites[spritenum];
+    }
+}
